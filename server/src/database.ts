8b6043d4cea3bababe179c0462f023a5c11ff837
Roman Rogowski <roman.j.rogowski@gmail.com>
2026-05-22 14:41:16 -0400

    fix: add mode environment variable

--- server/src/database.ts
+++ server/src/database.ts
@@ -4,7 +4,7 @@ import { Connector, IpAddressTypes } from "@google-cloud/cloud-sql-connector";
 const connector = new Connector();
 
 const clientOpts =
-  import.meta.env.MODE === "production"
+  process.env.MODE === "production"
     ? await connector.getOptions({
         instanceConnectionName: "nuance-c9944:us-east4:bhvr-pg-test",
         ipType: IpAddressTypes.PUBLIC,
